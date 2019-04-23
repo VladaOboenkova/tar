3952b83716c749710656438aa3d4b2724338a5e0
Влада <vladaoboenkova@gmail.com>
2019-04-23 16:59:53 +0300

    1/

--- src/main/java/classes/Logic.java
+++ src/main/java/classes/Logic.java
@@ -1,7 +1,6 @@
 package classes;
 
 import org.kohsuke.args4j.*;
-
 import java.io.File;
 import java.util.*;
 
@@ -12,12 +11,12 @@ public class Logic {
     }
 
     @Argument
-    private List<File> files = new ArrayList<>();
+    private List<File> files = new ArrayList<File>();
+
+    public File getOut() {
+        return out;
+    }
 
     @Option(name="-out")
-    private File globalFile = new File("Global File");
-
-
-
-
+    private File out = new File("output.txt");
 }
--- src/main/java/classes/tar.java
+++ src/main/java/classes/tar.java
@@ -6,8 +6,19 @@ import org.kohsuke.args4j.*;
 
 public class tar {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
 
+        Logic arguments = new Logic();
+        CmdLineParser parser = new CmdLineParser(arguments);
+
+        try {
+            parser.parseArgument(args);
+        } catch (CmdLineException e) {
+            System.err.println("e = " + e.toString());
+            parser.printUsage(System.out);
+        }
+        System.out.println(arguments.getFiles());
+        System.out.println(arguments.getOut());
     }
 }
 
