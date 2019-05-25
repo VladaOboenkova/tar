412c8efce07214832945ef0c1ec42ba5fc349ea2
Влада <vladaoboenkova@gmail.com>
2019-05-25 23:48:39 +0300

    final

---
+++ src/test/java/classes/LogicTest.java
@@ -0,0 +1,99 @@
+
+package classes;
+
+import org.apache.commons.io.*;
+import org.junit.*;
+import java.io.*;
+import java.util.*;
+
+public class LogicTest {
+
+    @Test
+    public void load() throws IOException {
+        Logic logic = new Logic();
+        File firstFile = new File("Texts/text1.txt");
+        File secondFile = new File("Texts/text2.txt");
+
+        List<File> files = new ArrayList<>();
+        files.add(firstFile);
+        files.add(secondFile);
+        logic.setFiles(files);
+        List<String> result = logic.load();
+
+        List<String> expected = new ArrayList<>();
+
+        File firstTestFile = new File("input (Test)/text1.txt");
+        File secondTestFile = new File("input (Test)/text2.txt");
+        List<File> testFiles = new ArrayList<>();
+        testFiles.add(firstTestFile);
+        testFiles.add(secondTestFile);
+
+        for (int i = 0; i < testFiles.size(); i++){
+            FileReader fileReader = new FileReader(testFiles.get(i));
+            char[] charText = new char[(int) testFiles.get(i).length()];
+            fileReader.read(charText);
+            String stringText = new String(charText);
+            if (i != testFiles.size() - 1){
+                stringText += "!end!" ;
+            }
+            expected.add(stringText);
+            fileReader.close();
+        }
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void out() throws IOException {
+        Logic logic = new Logic();
+
+        File expected = new File("input (Test)/output.txt");
+
+        List<String> strings = new ArrayList<>();
+        File firstTestFile = new File("input (Test)/text1.txt");
+        File secondTestFile = new File("input (Test)/text2.txt");
+        List<File> testFiles = new ArrayList<>();
+        testFiles.add(firstTestFile);
+        testFiles.add(secondTestFile);
+
+        for (int i = 0; i < testFiles.size(); i++){
+            File file = testFiles.get(i);
+            FileReader fileReader = new FileReader(file);
+            char[] charText = new char[(int) file.length()];
+            fileReader.read(charText);
+            String stringText = new String(charText);
+            String endText;
+            endText = stringText;
+            if (i != testFiles.size() - 1){
+                endText += "!end!";
+            }
+            strings.add(endText);
+            fileReader.close();
+        }
+
+        File result = new File("input (Test)/test.txt");
+
+        if (result.length() == 0){
+            result = logic.out(strings, result);
+        }
+        Assert.assertEquals(FileUtils.readLines(expected), FileUtils.readLines(result));
+    }
+
+    @Test
+    public void split() throws IOException {
+        Logic logic = new Logic();
+
+        File firstTestFile = new File("input (Test)/text1.txt");
+        File secondTestFile = new File("input (Test)/text2.txt");
+
+        List<File> testFiles = new ArrayList<>();
+        testFiles.add(firstTestFile);
+        testFiles.add(secondTestFile);
+
+        File test = new File("input (Test)/output.txt");
+        logic.setFileName(test);
+
+        List<File> result = logic.split();
+
+        Assert.assertEquals(testFiles, result);
+    }
+}
